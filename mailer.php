259c252081e780b085420a588653e39941d8fdd2
Emi-C <emil.costanzo@gmail.com>
2018-12-04 11:31:07 +0100

    formatting

--- mailer.php
+++ mailer.php
@@ -1,18 +1,15 @@
-<?php
-$name=$_POST["name"];
-$mail=$_POST["mail"];
-$msg=$_POST["msg"];
-
-
-$headers = 'From: Emiliano Costanzo <no-reply@emilianocostanzo.com>';
-
-echo $headers;
-
-$sent=mail( 'emil.costanzo@gmail.com', 'Nuovo messaggio da '.$name.' - '.$mail, $msg, $headers);
-
-if ( !$sent ) {
-   http_response_code(400);
-} else {
-   http_response_code(200);
-}
-?>
+<?php
+$name=$_POST["name"];
+$mail=$_POST["mail"];
+$msg=$_POST["msg"];
+
+$headers = 'From: Emiliano Costanzo <no-reply@emilianocostanzo.com>';
+
+$sent=mail( 'emil.costanzo@gmail.com', 'Nuovo messaggio da '.$name.' - '.$mail, $msg, $headers);
+
+if ( !$sent ) {
+   http_response_code(400);
+} else {
+   http_response_code(200);
+}
+?>
